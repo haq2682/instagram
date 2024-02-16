264129cd74ab3c1349acd6ccc8852a2de0e72951
haq2682 <haq2682@gmail.com>
2024-02-16 20:26:03 +0500

    Working on settings page

--- src/components/HelpSettings.js
+++ src/components/HelpSettings.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 export default function HelpSettings() {
     const [issueSubject, setIssueSubject] = useState('');
     const [issueDescription, setIssueDescription] = useState('');
+    const [issueImages, setIssueImages] = useState([]);
 
     const handleSubject = (event) => {
         setIssueSubject(event.target.value);
@@ -11,6 +12,13 @@ export default function HelpSettings() {
 
     const handleDescription = (event) => {
         setIssueDescription(event.target.value)
+    }
+
+    const handleImageUpload = (event) => {
+        setIssueImages([...issueImages, event.target.files]);
+        issueImages.map((img) => {
+            console.log(img.File);
+        })
     }
     return (
         <div className="help">
@@ -23,15 +31,20 @@ export default function HelpSettings() {
                            className="shadow-md rounded-xl" endContent={(<div className="text-gray-500">{issueSubject.length}/100</div>)}/>
                     <Textarea onChange={handleDescription} className="mt-4 shadow-md rounded-xl" variant="bordered" size="lg"
                               placeholder="Description of the issue"/>
-                    <input type="file" id="issueImage" name="issueImage" className="hidden"/>
-                    <Button for="issueImage" className="mt-8 float-left shadow-md bg-blue-400 text-white font-bold text-lg">
-                        Add a photo of the issue
+                    <input onChange={handleImageUpload} type="file" multiple id="issueImage" name="issueImage" className="hidden" accept="image/gif, image/jpeg, image/png, image/heic, image/hevc, image/webp"/>
+                    <Button className="mt-8 float-left shadow-md bg-blue-400 text-white font-bold text-lg">
+                        <label htmlFor="issueImage" className="py-8 px-8 cursor-pointer w-full">
+                            Add a photo of the issue
+                        </label>
                     </Button>
                     <Button className="mt-8 float-right shadow-md bg-green-400 text-white font-bold text-lg">
                         Submit
                     </Button>
                 </div>
             </form>
+            <div className="mt-24 flex flex-wrap">
+                <img src="https://cdn.pixabay.com/photo/2022/01/28/18/32/leaves-6975462_1280.png" alt="issue" className="h-32 w-32 object-cover"/>
+            </div>
         </div>
     );
 }
