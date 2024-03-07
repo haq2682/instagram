7f828904bf008fdf8842677e1f19913885d968ce
haq2682 <haq2682@gmail.com>
2024-03-07 09:57:53 +0500

    Working on email verification

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Search from './pages/Search';
 import Message from './pages/Message';
 import Saved from './pages/Saved';
 import Profile from './pages/Profile';
+import VerifyEmail from './pages/VerifyEmail';
+import VerifiedEmail from './pages/VerifiedEmail';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import {useDispatch, useSelector} from 'react-redux';
 import {useEffect, useState} from "react";
 import {authenticate} from './redux/authSlice';
@@ -15,7 +19,9 @@ import WelcomeLoader from "./components/WelcomeLoader";
 
 function App() {
     const [loader, setLoader] = useState(true);
-    const is_authenticated = useSelector(state => state.auth.is_authenticated);
+    // const is_authenticated = useSelector(state => state.auth.is_authenticated);
+    const is_authenticated = useState(true);
+    const is_verified = useState(false);
     const dispatch = useDispatch();
     useEffect(() => {
         setLoader(true);
@@ -30,18 +36,79 @@ function App() {
                 setTimeout(()=>setLoader(false), 1000);
             })
     }, []);
+
+    function getPage(page) {
+        switch(page) {
+            case "settings":
+                if(is_authenticated === true && is_verified === true) return <Settings/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+                break;
+
+            case "explore":
+                if(is_authenticated === true && is_verified === true) return <ExplorePosts/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "search":
+                if(is_authenticated === true && is_verified === true) return <Search/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "messages":
+                if(is_authenticated === true && is_verified === true) return <Message/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "saved":
+                if(is_authenticated === true && is_verified === true) return <Saved/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "profile":
+                if(is_authenticated === true && is_verified === true) return <Profile/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "verify":
+                if(is_authenticated === true && is_verified === false) return <VerifyEmail/>
+                else if(is_authenticated === true && is_verified === true) return <Navigate to={"/verified"}/>
+                else return <Navigate to={"/"}/>
+
+            case "verified":
+                if(is_authenticated === true && is_verified === true) return <VerifiedEmail/>
+                else if(is_authenticated === true && is_verified === false) return <Navigate to={"/verify"}/>
+                else return <Navigate to={"/"}/>
+
+            case "forgotPassword":
+                if(is_authenticated === false) return <ForgotPassword/>
+                else return <Navigate to={"/"}/>
+
+            case "resetPassword":
+                if(is_authenticated === false) return <ResetPassword/>
+                else return <Navigate to={"/"}/>
+                break;
+
+            default:
+                break;
+        }
+    }
   return (
       <main>
           {loader ? <WelcomeLoader/> : null}
           <BrowserRouter>
               <Routes>
                   <Route index element={<Home/>}/>
-                  <Route path="settings" element={is_authenticated ? <Settings/> : <Navigate to={"/"}/>}/>
-                  <Route path="explore" element={is_authenticated ? <ExplorePosts/> : <Navigate to={"/"}/>}/>
-                  <Route path="search" element={is_authenticated ? <Search/> : <Navigate to={"/"}/>}/>
-                  <Route path="messages" element={is_authenticated ? <Message/> : <Navigate to={"/"}/>}/>
-                  <Route path="saved" element={is_authenticated ? <Saved/> : <Navigate to={"/"}/>}/>
-                  <Route path="profile" element={is_authenticated ? <Profile/> : <Navigate to={"/"}/>}/>
+                  <Route path="settings" element={getPage("settings")}/>
+                  <Route path="explore" element={getPage("explore")}/>
+                  <Route path="search" element={getPage("search")}/>
+                  <Route path="messages" element={getPage("messages")}/>
+                  <Route path="saved" element={getPage("saved")}/>
+                  <Route path="profile" element={getPage("profile")}/>
+                  <Route path="verify" element={getPage("verify")}/>
+                  <Route path="verified" element={getPage("verified")}/>
+                  <Route path="forgotPassword" element={getPage("forgotPassword")}/>
+                  <Route path="resetPassword" element={getPage("resetPassword")}/>
               </Routes>
           </BrowserRouter>
       </main>
---
+++ src/pages/ResetPassword.js
@@ -0,0 +1,9 @@
+export default function ResetPassword() {
+    return (
+        <div className="h-screen">
+            <div className="welcome-background flex justify-center items-center h-full w-screen flex-col">
+                hello
+            </div>
+        </div>
+    );
+}
