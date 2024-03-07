fba2422d23fd23840d49dfe93064a1333e661852
haq2682 <haq2682@gmail.com>
2024-03-07 09:57:53 +0500

    Working on email verification

---
+++ src/pages/VerifyEmail.js
@@ -0,0 +1,9 @@
+export default function VerifyEmail() {
+    return (
+        <div className="h-screen">
+            <div className="welcome-background flex justify-center items-center h-full w-screen flex-col">
+                hello
+            </div>
+        </div>
+    );
+}
