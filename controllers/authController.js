982723bc621f0a852fa489309b221f162ae2b73c
haq2682 <haq2682@gmail.com>
2024-03-13 23:34:31 +0500

    Implemented JWT Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/User');
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     register: async (req, res) => {
@@ -20,29 +21,35 @@ module.exports = {
             res.status(500).json(error);
         }
     },
-    login: async (email, password, done) => {
+    login: async (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
         const user = await UserModel.findOne({email}).exec();
-        if(!user) return done(null, false, {message: "User not found"});
-        if(await bcrypt.compare(password, user.password)) return done(null, user);
-        else return done(null, false);
+        if(!user) return res.status(401).json({message: "User not found"});
+        if(!await bcrypt.compare(password, user.password)) return res.status(401).json({message: "Incorrect Password"});
+        else {
+            const token = jwt.sign({user}, 'Now I am become death, the destroyer of worlds', {expiresIn: '1w'});
+            res.cookie('token', token, {httpOnly: true, sameSite: 'none', secure: true});
+            return res.status(200).json(user);
+        }
     },
-    logout: (req, res, next) => {
-        req.logout(function (error) {
-            if(error) return next(error);
-            req.session.destroy(function(err){
-                if(err){
-                    console.log("error: " + err);
-                    res.status(500).json({message: "Error destroying session"});
-                } else {
-                    console.log("destroying session");
-                    res.clearCookie('mcookie');
-                    res.status(200).json({message: "Logged out successfully"});
-                }
-            });
-        })
+    logout: (req, res) => {
+        res.clearCookie('token');
+        res.status(200).json({message: "User logged out successfully"});
     },
-    getUser: (req, res) => {
-        res.send(req.user);
+    getUser: async (req, res) => {
+        try {
+            const token = req.cookies.token;
+            if(!token) return res.status(401).json({message: "User is not logged in"});
+
+            const {user} = jwt.verify(token, 'Now I am become death, the destroyer of worlds');
+            const userFromDB = await UserModel.find({email: user.email});
+            if(!userFromDB) return res.status(401).json({message: "User not found"});
+            res.send(userFromDB);
+        }
+        catch(error) {
+            res.status(500).json({message: error});
+        }
     },
     verifyEmail: async (req, res) => {
         const token = req.params.verify_token;
